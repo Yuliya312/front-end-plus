905b2017e3be0b543ef7a4029a12aa51824370e8
Yuliya312 <yuliya_312@ukr.net>
2018-08-02 22:05:41 +0300

    hw lec02 2-3 done

--- lections/lection_02_Async JS/homework/homework3/js3.js
+++ lections/lection_02_Async JS/homework/homework3/js3.js
@@ -4,124 +4,57 @@
 //  паузе (читаем youtube API). Вся страница служит для временного хранения видео, 
 //  которы вы запланировали посмотреть. Есть также кнопка - очистить всё.
 
-console.log(10);
-
 window.onload = function() {
 
-    var btnAddLocal = document.querySelector('#local_add'),
-        video_header =  document.querySelector('.video_header')
-        videoList = document.querySelector('#collection');
-        console.log(btnAddLocal);
-        console.log(videoList);  
+    const inputUrl = document.querySelector('#input_url'),
+          btnAddLocal = document.querySelector('#local_add'),
+          btnClear = document.querySelector('#clear'),
+          videoList = document.querySelector('#collection');
+        // console.log(btnAddLocal);
+        // console.log(videoList);  
+            btnClear.addEventListener('click', clear);
+            btnAddLocal.addEventListener('click', addVideoUrl);
 
-        // function setVideoData(){
-        //     localStorage.setItem('list', 'https://www.youtube.com/embed/M3GaKtQChF8');
-        //     // console.log(localvalue);
-        // }
+        let list = JSON.parse(localStorage.getItem("list")) || [];
+            render();
+            
+            function addVideoUrl(){
+                let url = inputUrl.value;
+                let videoId = url.slice(url.length - 11);
+                console.log(videoId);
 
-        // function getVideoData(){
-        //     let localvalue = localStorage.getItem('list');
-        //     console.log(localvalue);
-        // }
+                    list.push(videoId);
+                    localStorage.setItem("list", JSON.stringify(list));
 
-        // setVideoData();
-        // getVideoData();
-    // var i = j = 0;
-    btnAddLocal.addEventListener('click', function(){
-        // var videoData = getVideoData() || {};
-        let value = document.querySelector('#input_url').value;
-        let list = JSON.parse(window.localStorage.list);
-        list.push(value);
-        window.localStorage.list = JSON.stringify(list);
-        console.log(list);
+                console.log(list);
 
-        render();
-    });
+                render();
+            }
 
-    function render(){
-        window.localStorage.list = window.localStorage.list || JSON.stringify([]);
-        // console.log(videoList.children);
-        console.dir(videoList);
-        // var video = {
-        //     id: list.lastBoardId,
-        //     name: 'Item_' + list.lastBoardId,
-        //     tasks: []
-        // };
+            function render(){    
+                    // console.log(list);
 
-        // newVideo.url=list.value;
-        var listLocal = JSON.parse(window.localStorage.list);
-        var newVideo = document.createElement('iframe');
-        newVideo.url = listLocal.value;   
-        videoList.appendChild (newVideo);
-        console.dir(newVideo);
-        // getVideoData();
-        // for(var item in listLocal){
-        //     localList.innerHTML += listLocal[item] + "<br/ >"
-        // }
-    }
+                    if(list) {
+                        list.map(item => {
+                        let    newVideo = document.createElement('iframe');
+                        videoList.appendChild (newVideo);
+                        newVideo.src=`https://www.youtube.com/embed/${item}`;
+                        newVideo.style=`height="300px"frameborder="0"allow="autoplay" 
+                                        encrypted-media "allowfullscreen`;                                
+                        }).join("")   
 
-    // render();
+                    } else {
+                        videoList.innerHTML = "NO DATA";
+                    }
+                    console.dir(videoList);
+                }
 
-    function getVideoData(){
-        // let localvalue = JSON.parse(localStorage.getItem('list');
-
-        return JSON.parse(localStorage.getItem('list'));
-      }
-      console.log(getVideoData());
-        // function getVideoData(){
-//     let localvalue = localStorage.getItem('list');
-//     console.log(localvalue);
-// }
-
-    function setVideoData(o){
-        console.log(o);
-    localStorage.setItem('url', JSON.stringify(o));
-    // return false;
-    }
-
+            function clear() {
+                localStorage.clear();
+                render();
+            }
 }
 
 
-// function addBoard(list, options) {
-//     list.lastBoardId++;
 
-//     var board = {
-//         id: list.lastBoardId,
-//         name: 'Item_' + list.lastBoardId,
-//         tasks: []
-//     };
-//     // if (errors){
-//     // return false;
-//     // }
-//     list.boards.push(board);
-//     _renderBoard(options.pattern, board, options.target);
-
-//     return true;
-// }
-
-    function addBoardf() {
-    
-        var content = document.querySelector('.content');
-        var newBoard = document.createElement('div');
-        newBoard.className = 'board';
-        var newBoards = content.querySelector('#newBoards');
-        content.insertBefore(newBoard, newBoards);
-        var board = document.querySelectorAll('.board');
-        for(var i=0;i<board.length;i++) {
-        board[i].dataset.counter = i;
-        }
-        // var board = document.querySelectorAll('.board');
-        console.dir(board);
-        addPlusBtn();
-        plusBtn();
-            function addPlusBtn() {
-                var plusAdd = document.createElement('div');
-                plusAdd.className = 'plus';
-                plusAdd.innerHTML = '&#10010;';
-                newBoard.appendChild(plusAdd);
-                // console.dir(plusAdd);
-            }
-        // var board = document.querySelectorAll('.board');
-        // console.dir(board);
-    }
 
